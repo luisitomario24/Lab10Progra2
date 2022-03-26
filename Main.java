80cf269d5e32c5bbfc7a00ed6c85f97899c9735c
luisitomario24 <luisitomario24@yahoo.com>
2022-03-25 18:13:33 -0600

    arreglo de pop menu

--- Main.java
+++ Main.java
@@ -89,17 +89,15 @@ public class Main extends javax.swing.JFrame implements Serializable {
     private void initComponents() {
 
         popmenu = new javax.swing.JPopupMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
+        planeta1 = new javax.swing.JMenuItem();
+        Planeta2 = new javax.swing.JMenuItem();
         jProgressBar1 = new javax.swing.JProgressBar();
         jProgressBar2 = new javax.swing.JProgressBar();
         jCheckBox1 = new javax.swing.JCheckBox();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTree1 = new javax.swing.JTree();
         planeta2 = new javax.swing.JTextField();
-        planeta1 = new javax.swing.JTextField();
+        jtplaneta1 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         comboCientifico = new javax.swing.JComboBox<>();
         jLabel2 = new javax.swing.JLabel();
@@ -107,16 +105,21 @@ public class Main extends javax.swing.JFrame implements Serializable {
         jButton2 = new javax.swing.JButton();
         nombreCientifico = new javax.swing.JTextField();
 
-        jMenuItem1.setText("jMenuItem1");
-        popmenu.add(jMenuItem1);
+        planeta1.setText("planeta1");
+        planeta1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                planeta1ActionPerformed(evt);
+            }
+        });
+        popmenu.add(planeta1);
 
-        jCheckBoxMenuItem1.setSelected(true);
-        jCheckBoxMenuItem1.setText("jCheckBoxMenuItem1");
-        popmenu.add(jCheckBoxMenuItem1);
-
-        jMenuItem4.setText("planeta1\n");
-
-        jMenuItem2.setText("planeta2");
+        Planeta2.setText("Planeta2\n");
+        Planeta2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Planeta2ActionPerformed(evt);
+            }
+        });
+        popmenu.add(Planeta2);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -173,7 +176,7 @@ public class Main extends javax.swing.JFrame implements Serializable {
                                 .addComponent(jCheckBox1)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(planeta1, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
+                            .addComponent(jtplaneta1, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
                             .addComponent(planeta2)
                             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(comboCientifico, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -198,7 +201,7 @@ public class Main extends javax.swing.JFrame implements Serializable {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(planeta1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jtplaneta1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(44, 44, 44)
@@ -268,6 +271,26 @@ public class Main extends javax.swing.JFrame implements Serializable {
             vaciarArbol();
         }
     }//GEN-LAST:event_jCheckBox1ItemStateChanged
+
+    private void planeta1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_planeta1ActionPerformed
+        DefaultTreeModel model1 =(DefaultTreeModel)jTree1.getModel();
+        DefaultMutableTreeNode seleccion =(DefaultMutableTreeNode)jTree1.getSelectionPath().getLastPathComponent();
+        if(seleccion.getUserObject() instanceof Planeta){
+            seleccionado1=(Planeta)seleccion.getUserObject();
+        }
+        Planeta planeta2 =seleccionado2;
+        jtplaneta1.setText(planeta2.getNombre());
+    }//GEN-LAST:event_planeta1ActionPerformed
+
+    private void Planeta2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Planeta2ActionPerformed
+        DefaultTreeModel model1 =(DefaultTreeModel)jTree1.getModel();
+        DefaultMutableTreeNode seleccion2 =(DefaultMutableTreeNode)jTree1.getSelectionPath().getLastPathComponent();
+        if(seleccion2.getUserObject() instanceof Planeta){
+            seleccionado2 =(Planeta)seleccion2.getUserObject();
+        }
+        Planeta planeta1= seleccionado2;
+        jtplaneta1.setText(planeta1.getNombre());
+    }//GEN-LAST:event_Planeta2ActionPerformed
     public double calcularDistancia() {
         return 0 ;
     }
@@ -305,22 +328,20 @@ public class Main extends javax.swing.JFrame implements Serializable {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem Planeta2;
     private javax.swing.JButton agregarCientifico;
     private javax.swing.JComboBox<String> comboCientifico;
     private javax.swing.JButton jButton2;
     private javax.swing.JCheckBox jCheckBox1;
-    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JProgressBar jProgressBar1;
     private javax.swing.JProgressBar jProgressBar2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTree jTree1;
+    private javax.swing.JTextField jtplaneta1;
     private javax.swing.JTextField nombreCientifico;
-    private javax.swing.JTextField planeta1;
+    private javax.swing.JMenuItem planeta1;
     private javax.swing.JTextField planeta2;
     private javax.swing.JPopupMenu popmenu;
     // End of variables declaration//GEN-END:variables
@@ -332,4 +353,6 @@ public class Main extends javax.swing.JFrame implements Serializable {
 //    private Planeta Saturno=new Planeta();
 //    private Planeta Urano=new Planeta();
 //    private Planeta Neptuno=new Planeta();
+    private Planeta seleccionado2=new Planeta();
+    private Planeta seleccionado1=new Planeta();
 }
